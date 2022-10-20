8acb4caf16f318a523a46a13bce4f90dc9a4371f
MaximLaskin <maxlaskin@mail.ru>
2022-10-21 00:55:37 +0500

    Initial Commit

---
+++ SliderUI/SliderUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderUIApp.swift
+//  SliderUI
+//
+//  Created by Maxim on 21.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
