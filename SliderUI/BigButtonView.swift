5b8342fad4d0442b3af827af795549c10e1ed1df
MaximLaskin <maxlaskin@mail.ru>
2022-10-21 15:03:34 +0500

    Настройка кнопки

---
+++ SliderUI/BigButtonView.swift
@@ -0,0 +1,31 @@
+//
+//  BigButtonView.swift
+//  SliderUI
+//
+//  Created by Maxim on 21.10.2022.
+//
+
+import SwiftUI
+
+struct BigButtonView: View {
+    let title: String
+    let action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .bold()
+                .frame(width: 200, height: 50)
+                .foregroundColor(.indigo)
+                .background(LinearGradient(colors: [.mint, .white], startPoint: .bottomLeading, endPoint: .topTrailing))
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .shadow(radius: 4)
+        }
+    }
+}
+
+struct BigButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        BigButtonView(title: "Reset", action: {})
+    }
+}
--- SliderUI/ContentView.swift
+++ SliderUI/ContentView.swift
@@ -8,10 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    //    @State private var colorView: Color
+
+    @State private var sliderValue = Double.random(in: 0...1)
+
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        VStack {
+            Rectangle()
+                .foregroundColor(.blue)
+                .frame(height: 140)
+                .cornerRadius(10)
+
+            HStack {
+                Text("1")
+                Slider(value: $sliderValue)
+                Color(.green)
+                    .frame(width: 30, height: 30)
+
+            }
+            Button {
+                print("Button pressed!")
+            } label: {
+                Text("Reset")
+                    .bold()
+                    .frame(width: 200, height: 50)
+                    .foregroundColor(.indigo)
+                    .background(LinearGradient(colors: [.mint, .white], startPoint: .bottomLeading, endPoint: .topTrailing))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .shadow(radius: 4)
+
+
+            }
+            Spacer()
+
+        }
+        .padding()
     }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
