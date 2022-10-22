b0c3f3cd0e4da6e4037408fd1f1b4c97c1f46dc0
MaximLaskin <maxlaskin@mail.ru>
2022-10-22 16:09:32 +0500

    Настройка View

--- SliderUI/ContentView.swift
+++ SliderUI/ContentView.swift
@@ -13,26 +13,40 @@ struct ContentView: View {
     @State private var greenSliderValue = Double.random(in: 0...255)
     @State private var blueSliderValue = Double.random(in: 0...255)
 
+    @State private var redTextField = ""
+    @State private var greenTextField = ""
+    @State private var blueTextField = ""
+
     var body: some View {
 
         ZStack {
             LinearGradient(colors: [.mint, .white ], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
             VStack {
                 Rectangle()
                     .foregroundColor(
                         Color(
-                        red: redSliderValue/255,
-                        green: greenSliderValue/255,
-                        blue: blueSliderValue/255,
-                        opacity: 1))
+                            red: redSliderValue/255,
+                            green: greenSliderValue/255,
+                            blue: blueSliderValue/255,
+                            opacity: 1))
                     .frame(height: 140)
                     .cornerRadius(10)
                     .shadow(radius: 5)
-
-
-                ColorSliderView(value: $redSliderValue)
-                ColorSliderView(value: $greenSliderValue)
-                ColorSliderView(value: $blueSliderValue)
+                VStack {
+                    ColorSliderView(
+                        value: $redSliderValue,
+                        color: .red,
+                        textFieldValue: $redTextField)
+                    ColorSliderView(
+                        value: $greenSliderValue,
+                        color: .green,
+                        textFieldValue: $greenTextField)
+                    ColorSliderView(
+                        value: $blueSliderValue,
+                        color: .blue,
+                        textFieldValue: $blueTextField)
+                }
 
                 BigButtonView(title: "Reset") {
                     redSliderValue = 0
@@ -40,19 +54,34 @@ struct ContentView: View {
                     blueSliderValue = 0
                 }
                 Spacer()
-            }
-            .padding()
-        }.ignoresSafeArea()
-    }
+                    .frame(height: 200)
+                /* без height элементы расползаются на весь экран, почему?
+        по задумке слайдеры должны быть близко друг к другу, на симуляторе 11 онидалеко друг от друга.
+                */
+            }.padding()
+        }
 
+    }
 }
 struct ColorSliderView: View {
     @Binding var value: Double
+    let color: Color
+    @Binding var textFieldValue:String
 
     var body: some View {
         HStack {
             Text("1").font(.subheadline)
-            Slider(value: $value, in: 0...255, step: 1) //диапазон и шаг
+            ZStack {
+                LinearGradient(
+                    gradient: Gradient(colors: [.black, color]),
+                    startPoint: .leading,
+                    endPoint: .trailing
+                )
+                .mask(Slider(value: $value, in: 0...255, step: 1))
+
+                Slider(value: $value, in: 0...255, step: 1)
+                    .opacity(0.05)
+            }
             ZStack {
                 Color(.green)
                     .frame(width: 45, height: 30)
@@ -60,6 +89,10 @@ struct ColorSliderView: View {
                     .shadow(radius: 4)
                 Text(lround(value).formatted()).font(.subheadline)
             }
+            TextField("", text: $textFieldValue)
+                .textFieldStyle(.roundedBorder)
+                .frame(width: 42)
+
         }
     }
 }
